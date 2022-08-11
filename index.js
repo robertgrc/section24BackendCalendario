3a9bcaa590745e0b1c0c212babf229f45d981493
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-08-11 19:25:41 -0400

    Configurar CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { dbConnection } = require("./database/config");
 require("dotenv").config();
 
@@ -7,6 +8,9 @@ const app = express();
 
 //Base de datos
 dbConnection();
+
+//CORS
+app.use(cors());
 
 //Directorio Publico
 app.use(express.static("public"));
